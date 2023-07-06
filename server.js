31029ad1532e1c66f37453d6182cef90556a2184
nyuieva <ertonqwesi@gmail.com>
2023-07-06 09:55:56 -0700

    local uploads

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
 const prisma = new PrismaClient();
 
 app.use(cors({ origin: "*" }));
-
+ 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
